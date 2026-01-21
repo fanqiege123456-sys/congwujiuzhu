079440d215db7e44800070b05370de123505c923
fanqiege123456-sys <fanqiege123456@gmail.com>
2026-01-21 14:43:56 +0800

    Upload types.js via GitNetDisk

---
+++ chongwujiuzhu/congwujiuzhu/types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PetStatus = void 0;
+var PetStatus;
+(function (PetStatus) {
+    PetStatus["NEEDS_RESCUE"] = "NEEDS_RESCUE";
+    PetStatus["RESCUED"] = "RESCUED";
+})(PetStatus || (exports.PetStatus = PetStatus = {}));
